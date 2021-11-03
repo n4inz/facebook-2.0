9a1d8dda9194c5c8fefe498503273c90d78b3345
n4inz <nainmo@ymail.com>
2021-11-03 18:33:52 +0800

    Membuat Feed

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,16 @@
 // import 'tailwindcss/tailwind.css'
+import { Provider } from "next-auth/client"
+
 import '../globals.css';
+
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <Provider session={pageProps.session}>
+      <Component {...pageProps} />
+
+    </Provider>
+    
+  )
 }
 
 export default MyApp
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,12 @@
 import Head from 'next/head'
 import Header from '../components/Header'
+import Login from '../components/Login';
+import { getSession } from "next-auth/client";
+import Sidebar from '../components/Sidebar';
+import Feeds from '../components/Feeds';
 
-export default function Home() {
+export default function Home({session}) {
+  if(!session) return <Login />;
   return (
     <div>
       <Head>
@@ -11,10 +16,24 @@ export default function Home() {
 
       {/* Header */}
       <Header/>
-      <main>
-
+      <main className="flex">
+        {/* Sidebar */}
+        <Sidebar/>
+        {/* Feed */}
+        <Feeds />
+        {/* Widgets */}
       </main>
 
     </div>
   )
+}
+
+export async function getServerSideProps(context){
+  const session = await getSession(context);
+
+  return {
+    props: {
+      session
+    }
+  }
 }
